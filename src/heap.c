8d71b1cd94414adc85a674872bf47670a2a1ee91
staplp2 <staplp2@rpi.edu>
2022-09-19 19:18:04 -0400

    Update heap.c
    Push for homework 1; added leak detection using mem_leak_tracker_t struct

--- src/heap.c
+++ src/heap.c
@@ -6,8 +6,10 @@
 #include <stddef.h>
 #include <stdio.h>
 
+
 #define WIN32_LEAN_AND_MEAN
 #include <windows.h>
+#include <dbghelp.h>
 
 typedef struct arena_t
 {
@@ -15,12 +17,26 @@ typedef struct arena_t
 	struct arena_t* next;
 } arena_t;
 
+
+//Structure to track memory accesses and look out for leaks
+typedef struct mem_leak_tracker_t
+{
+	void* address; //address of memory access
+	int free_state; //whether or not the memory at the address is currently freed
+	size_t block_size; //size of memory being used at address
+	void* call_stack [64]; //call stack from where memory was initially allocated
+	int call_stack_size; //number of items on call stack
+
+} mem_leak_tracker_t;
+
 typedef struct heap_t
 {
 	tlsf_t tlsf;
 	size_t grow_increment;
 	arena_t* arena;
+	mem_leak_tracker_t leak_tracker_list[64];
 } heap_t;
+
 
 heap_t* heap_create(size_t grow_increment)
 {
@@ -67,16 +83,72 @@ void* heap_alloc(heap_t* heap, size_t size, size_t alignment)
 
 		address = tlsf_memalign(heap->tlsf, alignment, size);
 	}
+	mem_leak_tracker_t leak_tracker = { .address = address, .block_size = size, .free_state = 1, };
+	unsigned short frames = CaptureStackBackTrace(1, 63, leak_tracker.call_stack, NULL);
+	leak_tracker.call_stack_size = frames;
+
+	for (int i = 0; i < 64; i++) {
+		if (heap->leak_tracker_list[i].free_state == 1) {
+			continue;
+		}
+		else {
+			heap->leak_tracker_list[i] = leak_tracker;
+			break;
+		}
+	}
+
 	return address;
 }
 
 void heap_free(heap_t* heap, void* address)
 {
 	tlsf_free(heap->tlsf, address);
+	for (int i = 0; i < 64; i++) {
+		if (heap->leak_tracker_list[i].address == address) {
+			heap->leak_tracker_list[i].block_size = 0;
+			heap->leak_tracker_list[i].free_state = 0;
+			return;
+		}
+	}
 }
 
 void heap_destroy(heap_t* heap)
 {
+	for (int i = 0; i < 64; i++) {
+		if (heap->leak_tracker_list[i].free_state == 1) {
+
+			debug_print(
+				k_print_error,
+				"Memory leak of size %d bytes with callstack:\n",
+				heap->leak_tracker_list[i].block_size);
+
+			SymInitialize(GetCurrentProcess(), NULL, TRUE);
+			
+			SYMBOL_INFO* symbol = (SYMBOL_INFO*)calloc(sizeof(SYMBOL_INFO) + 256 * sizeof(char), 1);
+			symbol->MaxNameLen = 255;
+			symbol->SizeOfStruct = sizeof(SYMBOL_INFO);
+
+			for (int j = 0; j < (heap->leak_tracker_list[i].call_stack_size - 6); j++) {
+				if (SymFromAddr(GetCurrentProcess(), (DWORD64)(heap->leak_tracker_list[i].call_stack[j]), 0, symbol)) {
+					debug_print(
+						k_print_error,
+						"[%d] %s\n",
+						j, symbol->Name);
+				}
+				else {
+					debug_print(
+						k_print_error,
+						"Broken");
+					DWORD lasterror = GetLastError();
+					printf("\n    Error! - Error code %d\n", lasterror);
+				}
+				
+			}
+			free(symbol);
+			SymCleanup(GetCurrentProcess());
+		}
+	}
+
 	tlsf_destroy(heap->tlsf);
 
 	arena_t* arena = heap->arena;
